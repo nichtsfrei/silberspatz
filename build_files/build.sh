aa23ab676f61586e3a802e5915801b210c559b5f
Philipp Eder <philipp.eder@posteo.net>
2025-08-09 14:50:51 +0200

    Add: minor terminal programs

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y fish tmux neovim
+dnf5 install -y pass
+# maybe akready ibstalled?
+dnf5 install -y pam-u2f pamu2fcfg
 
 # Use a COPR Example:
 #
@@ -21,4 +24,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
