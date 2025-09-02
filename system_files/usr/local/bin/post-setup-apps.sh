e5f097f4b2fff9be48a378bd1eb6056679788d79
philipp eder <philipp.eder@posteo.net>
2025-09-02 19:57:16 +0200

    Add: keepass

--- system_files/usr/local/bin/post-setup-apps.sh
+++ system_files/usr/local/bin/post-setup-apps.sh
@@ -4,4 +4,5 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak install -y flathub io.gitlab.librewolf-community
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y flathub org.videolan.VLC
+flatpak install -y flathub org.keepassxc.KeePassXC
 
