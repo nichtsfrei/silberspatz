9f4f2c4e57990bc728b0f89c4577616912fb4dfd
Philipp Eder <philipp.eder@posteo.net>
2025-09-27 16:14:51 +0200

    moep

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,27 +2,14 @@
 
 set -ouex pipefail
 
-### Install packages
+# maybe install niri without recommendations
+# I don't use alacritty
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 remove -y firefox nano
 dnf5 install -y fish tmux neovim distrobox \
 	pam-u2f pamu2fcfg pamtester \
 	setroubleshoot \
 	@virtualization \
+	foot cosmic-app-library brightnessctl \
 	niri
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
-
-#systemctl enable podman.socket
+dnf5 remove -y firefox nano
--- system_files/usr/local/bin/post-setup-apps.sh
+++ system_files/usr/local/bin/post-setup-apps.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -ex
+set -x
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub io.gitlab.librewolf-community
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y flathub org.videolan.VLC
 flatpak install -y flathub org.keepassxc.KeePassXC
-
+flatpak install -y flathub org.pulseaudio.pavucontrol
