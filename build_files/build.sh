32ae9c829613e0f95182ae69ca8d7ac288fc72c1
Philipp Eder <philipp.eder@posteo.net>
2025-10-16 22:04:25 +0200

    KDE?

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ dnf5 install -y fish tmux neovim distrobox \
 	pam-u2f pamu2fcfg pamtester \
 	setroubleshoot \
 	@virtualization \
-	foot cosmic-app-library brightnessctl \
-	niri
+	just \
+	brightnessctl
 
 dnf5 remove -y firefox nano
