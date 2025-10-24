ab1642cf758a4908a96a08d5657aa72eb00783e4
Philipp Eder <philipp.eder@posteo.net>
2025-10-24 15:49:35 +0200

    Trying WM again ...

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,14 +2,14 @@
 
 set -ouex pipefail
 
-# maybe install niri without recommendations
-# I don't use alacritty
-
 dnf5 install -y fish tmux neovim distrobox \
+	NetworkManager-tui \
 	pam-u2f pamu2fcfg pamtester \
 	setroubleshoot \
 	@virtualization \
 	just \
-	brightnessctl
+	brightnessctl \
+	flatpak \
+	niri
 
 dnf5 remove -y firefox nano
