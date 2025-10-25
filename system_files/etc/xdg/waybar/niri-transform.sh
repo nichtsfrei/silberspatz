8d6babb653d50b06218e258454959ebb7ed8e540
Philipp Eder <philipp.eder@posteo.net>
2025-10-25 13:05:29 +0200

    Minor convenience

--- system_files/etc/xdg/waybar/niri-transform.sh
+++ system_files/etc/xdg/waybar/niri-transform.sh
@@ -5,7 +5,7 @@ output=$(niri msg --json outputs)
 current_transform_eDP1=$(echo "$output" | jq -r '.["eDP-1"].logical.transform')
 
 if [[ "$current_transform_eDP1" == "Normal" ]]; then
-    niri msg output "eDP-1" transform 270
+    niri msg output "eDP-1" transform 90
 else
     niri msg output "eDP-1" transform "normal"
 fi
